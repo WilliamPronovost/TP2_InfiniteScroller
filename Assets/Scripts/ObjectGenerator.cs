52ec742baf2cfae50b97dbeae0da0c785f85c3bd
William Pronovost <2430066@collegebart.ca>
2025-11-03 11:30:38 -0500

    Put music and sounds and make camera only able to follow player horizontaly

---
+++ Assets/Scripts/ObjectGenerator.cs
@@ -0,0 +1,50 @@
+//using NUnit.Framework;
+//using System.Runtime.CompilerServices;
+//using Unity.VisualScripting;
+//using UnityEngine;
+
+//public class ObjectGenerator : MonoBehaviour
+//{
+//    //[SerializeField] private GameObject[] m_platformPrefab = { };
+//    //[SerializeField] private Transform[] m_platformCache = { };
+//    [SerializeField] private GameObject m_platformPrefab;
+//    [SerializeField] private Transform m_player;
+//    [SerializeField] private Transform m_startingPlatform;
+//    [SerializeField] private float m_distanceForPlatformSpawn;
+
+//    private List<Transform> m_platformCache;
+//    [SerializeField] private int m_maxPlatformCount;
+
+//    private Transform m_lastCreatedPlatform;
+
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        m_platformCache = new List<Transform>();
+//        m_lastCreatedPlatform = m_startingPlatform;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if(Vector3.Distance(m_player.position, m_lastCreatedPlatform.position) < m_distanceForPlatformSpawn)
+//        {
+//            Transform platformEndPoint = m_lastCreatedPlatform.Find("End");
+//            m_lastCreatedPlatform = SpawnPosition(platformEndPoint.position);
+//            m_platformCache.Add(m_lastCreatedPlatform);
+
+//            if(m_platformCache.Count > m_maxPlatformCount)
+//            {
+//                Transform platformToDelete = m_platformCache[0];
+//                Destroy(platformToDelete.gameObject);
+//                m_platformCache.RemoveAt(0);
+//            }
+//        }
+//    }
+
+//    private Transform SpawnPosition(Vector3 spawnPosition)
+//    {
+//        GameObject newPlatform = Instantiate(m_platformPrefab, spawnPosition, Quaternion.identity);
+//        return newPlatform.transform;
+//    }
+//}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,7 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] private Animator m_playerAnimator;
     [SerializeField] private InputActionAsset m_inputFile;
+    [SerializeField] private Sprite m_playerRunSprite;
+    [SerializeField] private Sprite m_playerJumpSprite;
     [SerializeField] private float m_playerSpeed;
     [SerializeField] private float m_playerJumpForce;
     private bool m_isOnGround = true;
@@ -17,6 +20,7 @@ public class PlayerControls : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        m_playerAnimator = GetComponent<Animator>();
         m_playerRigidbody = GetComponent<Rigidbody2D>();
         m_moveAction = m_inputFile.FindAction("Move");
         m_jumpAction = m_inputFile.FindAction("Jump");
@@ -35,7 +39,7 @@ public class PlayerControls : MonoBehaviour
         m_elapsed += Time.deltaTime;
         if (m_elapsed >= m_delay)
         {
-            m_playerSpeed *= 1.1f;
+            m_playerSpeed *= 1.3f;
             m_elapsed = 0.0f;
         }
     }
@@ -44,11 +48,20 @@ public class PlayerControls : MonoBehaviour
         Vector3 calculatedVeclocity = m_playerRigidbody.linearVelocity;
         calculatedVeclocity.x = m_playerSpeed;
         m_playerRigidbody.linearVelocity = calculatedVeclocity;
-	}
+        if(calculatedVeclocity.magnitude > 0)
+        {
+            m_playerAnimator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            m_playerAnimator.SetBool("IsRunning", false);
+        }
+    }
     private void Jumping()
     {
         m_playerRigidbody.AddForce(Vector3.up * m_playerJumpForce, ForceMode2D.Impulse);
-	}
+        m_playerAnimator.SetBool("IsJumping", true);
+    }
     public void CollectCoin()
     {
         m_highScore+= 100;
