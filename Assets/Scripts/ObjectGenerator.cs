281aab008fca5853b0351ad3159e022cef9b1f35
William Pronovost <2430066@collegebart.ca>
2025-11-10 11:18:09 -0500

    Start platform list and start working on Game HUD

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
-    //[SerializeField] private GameObject[] m_platformPrefab = { };
+    [SerializeField] private GameObject[] m_platformTypes;
     //[SerializeField] private Transform[] m_platformCache = { };
     [SerializeField] private GameObject m_greenPlatformPrefab;
 	[SerializeField] private GameObject m_yellowPlatformPrefab;
@@ -21,20 +21,24 @@ public class ObjectGenerator : MonoBehaviour
     [SerializeField] private float m_distanceForPlatformSpawn;
     [SerializeField] private float m_intervalX;
     [SerializeField] private float m_platformLength;
-	[SerializeField] private float m_playerRange;
+    [SerializeField] private float m_playerRange;
 
 	private List<Transform> m_platformCache;
 	private List<Transform> m_coinCache;
-	[SerializeField] private int m_maxPlatformCount;
+    private List<Transform> m_sawCache;
+    [SerializeField] private int m_maxPlatformCount;
+    [SerializeField] private int m_currentPlatformCount; 
 	[SerializeField] private int m_maxCoinCount;
+    [SerializeField] private int m_maxSawCount;
 
-	private Transform m_lastCreatedPlatform;
+    private Transform m_lastCreatedPlatform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_platformCache = new List<Transform>();
         m_coinCache = new List<Transform>();
+        m_sawCache = new List<Transform>();
         m_lastCreatedPlatform = m_startingPlatform;
     }
 
@@ -44,18 +48,24 @@ public class ObjectGenerator : MonoBehaviour
         if (Vector3.Distance(m_player.position, m_lastCreatedPlatform.position) < m_distanceForPlatformSpawn)
         {
             Vector3 spawnDistance = new Vector3(m_intervalX, 0, 0);
+
             Transform platformEndPoint = m_lastCreatedPlatform.Find("End");
             float coinRangeX = Random.Range(m_greenPlatformPrefab.transform.position.x - m_platformLength, m_greenPlatformPrefab.transform.position.x + m_platformLength);
             float coinRangeY = Random.Range(m_greenPlatformPrefab.transform.position.y + 1, m_greenPlatformPrefab.transform.position.y + m_playerRange);
             Vector3 coinSpawningRange = new Vector3(coinRangeX, coinRangeY, 0);
-			if (m_player.position.x == m_spawner.position.x)
+            Vector3 coinSpawnDistance = platformEndPoint.position + coinSpawningRange + spawnDistance;
+
+            float sawRangeX = Random.Range(m_greenPlatformPrefab.transform.position.x - m_platformLength, m_greenPlatformPrefab.transform.position.x + m_platformLength);
+            float sawRangeY = m_greenPlatformPrefab.transform.position.y + 1.5f;
+            Vector3 sawSpawningRange = new Vector3(sawRangeX, sawRangeY, 0);
+            Vector3 sawSpawnDistance = platformEndPoint.position + sawSpawningRange + spawnDistance;
+            if (m_player.position.x == m_spawner.position.x)
             {
                 spawnDistance = Vector3.zero;
             }
-            m_lastCreatedPlatform = SpawnPosition(platformEndPoint.position + spawnDistance, platformEndPoint.position + coinSpawningRange);
+            m_lastCreatedPlatform = SpawnPosition(platformEndPoint.position + spawnDistance, coinSpawnDistance, sawSpawnDistance);
             m_platformCache.Add(m_lastCreatedPlatform);
             m_coinCache.Add(platformEndPoint);
-
             if (m_platformCache.Count > m_maxPlatformCount)
             {
                 Transform platformToDelete = m_platformCache[0];
@@ -68,13 +78,20 @@ public class ObjectGenerator : MonoBehaviour
 				Destroy(coinToDelete.gameObject);
 				m_coinCache.RemoveAt(0);
             }
+            if (m_sawCache.Count > m_maxSawCount)
+            {
+                Transform sawToDelete = m_sawCache[0];
+                Destroy(sawToDelete.gameObject);
+                m_sawCache.RemoveAt(0);
+            }
         }
     }
 
-    private Transform SpawnPosition(Vector3 spawnPosition, Vector3 coinPosition)
+    private Transform SpawnPosition(Vector3 spawnPosition, Vector3 coinPosition, Vector3 sawPosition)
     {
 		GameObject newCoin = Instantiate(m_coinPrefab, coinPosition, Quaternion.identity);
-		GameObject newPlatform = Instantiate(m_greenPlatformPrefab, spawnPosition, Quaternion.identity);
+        GameObject newSaw = Instantiate(m_sawPrefab, sawPosition, Quaternion.identity);
+        GameObject newPlatform = Instantiate(m_greenPlatformPrefab, spawnPosition, Quaternion.identity);
 	    float randomData = Random.Range(0f, 1f);
 		if (randomData == m_platformTypeSpawnChance)
 		{
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,7 +7,7 @@ public class PlayerControls : MonoBehaviour
     private Rigidbody2D m_playerRigidbody;
 
     [SerializeField] private InputActionAsset m_inputFile;
-	private InputAction m_moveAction;
+    private InputAction m_moveAction;
     private InputAction m_jumpAction;
 
     [SerializeField] private float m_playerSpeed;
@@ -16,8 +16,8 @@ public class PlayerControls : MonoBehaviour
     private float m_delay = 5.0f;
 
     [SerializeField] private float m_playerJumpForce;
-    [SerializeField] private float m_playerDoubleJumpForce;
-    private bool m_isOnGround = true;
+    private bool m_isOnGround;
+    private bool m_canJump;
 
     [SerializeField] private Sprite m_playerRunSprite;
     [SerializeField] private Sprite m_playerJumpSprite;
@@ -26,8 +26,8 @@ public class PlayerControls : MonoBehaviour
 
     [SerializeField] private AudioSource m_soundsSource;
     [SerializeField] private AudioClip m_jumpSFX;
-
-    private int m_jumpCount = 0;
+    bool spaceKeyPressed;
+    private int m_jumpCount;
     private int m_highScore = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,27 +36,18 @@ public class PlayerControls : MonoBehaviour
         m_playerRigidbody = GetComponent<Rigidbody2D>();
         m_soundsSource = GetComponent<AudioSource>();
 
+        m_isOnGround = false;
+        m_canJump = false;
+
         m_moveAction = m_inputFile.FindAction("Move");
         m_jumpAction = m_inputFile.FindAction("Jump");
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
-		Running();
-        bool spaceKeyPressed = m_jumpAction.WasPressedThisFrame();
-        if (spaceKeyPressed && m_isOnGround && m_jumpCount == 0)
-        {
-            Jumping();
-            m_isOnGround = false;
-            m_jumpCount += 1;
-            if(spaceKeyPressed && !m_isOnGround && m_jumpCount == 1)
-            {
-                DoubleJumping();
-                m_jumpCount -= 1;
-            }
-        }
-
+        Running();
+        Jumping();
         m_elapsed += Time.deltaTime;
         if (m_elapsed >= m_delay)
         {
@@ -65,46 +56,61 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
-	private void Running()
-	{
-		Vector3 calculatedVeclocity = m_playerRigidbody.linearVelocity;
-		calculatedVeclocity.x = m_playerSpeed;
-		m_playerRigidbody.linearVelocity = calculatedVeclocity;
-		if (calculatedVeclocity.magnitude > 0)
-		{
-			m_playerAnimator.SetBool("IsRunning", true);
-		}
-		else
-		{
-			m_playerAnimator.SetBool("IsRunning", false);
-		}
-	}
+    private void Running()
+    {
+        Vector3 calculatedVeclocity = m_playerRigidbody.linearVelocity;
+        calculatedVeclocity.x = m_playerSpeed;
+        m_playerRigidbody.linearVelocity = calculatedVeclocity;
+        if (calculatedVeclocity.magnitude > 0)
+        {
+            m_playerAnimator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            m_playerAnimator.SetBool("IsRunning", false);
+        }
+    }
 
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         m_isOnGround = true;
+        m_canJump = true;
         m_jumpCount = 0;
-	}
+    }
+
+    // Gael m'a aidé avec cette partie de mon code pour faire le double jump
+
     private void Jumping()
     {
-        m_soundsSource.clip = m_jumpSFX;
-        m_soundsSource.Play();
-		m_playerRigidbody.AddForce(Vector3.up * m_playerJumpForce, ForceMode2D.Impulse);
-        m_playerAnimator.SetBool("IsJumping", true);
-    }
-    private void DoubleJumping()
-    {
-        m_soundsSource.clip = m_jumpSFX;
-        m_soundsSource.Play();
-        m_playerRigidbody.AddForce(Vector3.up * m_playerDoubleJumpForce, ForceMode2D.Impulse);
-        m_playerAnimator.SetBool("IsJumping", true);
+        spaceKeyPressed = m_jumpAction.WasPressedThisFrame();
+        if (m_isOnGround && m_canJump && spaceKeyPressed)
+        {
+            m_soundsSource.clip = m_jumpSFX;
+            m_soundsSource.Play();
+            m_playerRigidbody.AddForce(Vector2.up * m_playerJumpForce, ForceMode2D.Impulse);
+            m_jumpCount += 1;
+            m_playerAnimator.SetBool("IsJumping", true);
+            m_isOnGround = false;
+            Debug.Log("Jumped");
+
+        }
+        else if (m_isOnGround == false && m_canJump && spaceKeyPressed && m_jumpCount >= 1)
+        {
+            m_soundsSource.clip = m_jumpSFX;
+            m_soundsSource.Play();
+            m_playerRigidbody.AddForce(Vector2.up * m_playerJumpForce, ForceMode2D.Impulse);
+            m_jumpCount += 1;
+            m_playerAnimator.SetBool("IsJumping", true);
+            m_canJump = false;
+            Debug.Log("Jumped");
+        }
     }
     public void CollectCoin()
     {
-        m_highScore+= 100;
-	}
+        m_highScore += 100;
+    }
     public void HitObstacle()
     {
         SceneManager.LoadScene("GameOver");
-	}
+    }
 }
